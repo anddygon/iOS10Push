8d56cddd89f77c0b77b349cda3a288eda503a683
xiaoP <anddygong@gmail.com>
2017-03-08 14:45:30 +0800

    新建项目

---
+++ iOS10Push/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  iOS10Push
+//
+//  Created by xiaoP on 2017/3/8.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
