0e7151c7480302452f95a1a36ad52dcf8ea86977
xiaoP <anddygong@gmail.com>
2017-03-08 14:47:27 +0800

    .

---
+++ Pods/Target Support Files/Pods-iOS10Push/Pods-iOS10Push-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS10Push : NSObject
+@end
+@implementation PodsDummy_Pods_iOS10Push
+@end
---
+++ Pods/Target Support Files/Pods-iOS10Push/Pods-iOS10Push-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_iOS10PushVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iOS10PushVersionString[];
+
